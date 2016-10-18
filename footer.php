ed5fd9d1ab1cc3149bf7e919da3f858c3bf9684d
Charo <diridolloumarwan@gmail.com>
2016-10-18 15:39:00 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
-<div class="container-footer">
+<div class="container">
 	<div id="footer" class="row slgrow">
-		<div class="col-md-4">
-			<H1>ouloulou</H1>		<!--Logo première colonne -->
+		<div class="col-md-4" id="logo" >
+			<img src="<?php bloginfo('url'); ?>/wp-content/themes/seloger/img/logo.png" width="250" height="250" alt="logo SLG" class="img-responsive">
+			<hr>
+			<h4>Copyright 2016 © Alexandre Herrero // Marwan Diridollou</h4>		<!--Logo première colonne -->
 		</div>
 				
 		<div class="col-md-2">
-			<h4>Accueil</h4>
+			<h3>Accueil</h3>
 				<a href="">Dépôt d'annonce</a><br>
 				<a href="">Trouver une location</a><br>
 				<a href="">Recherche du lieu</a><br>		<!--2 ième colonne -->
@@ -13,7 +15,7 @@
 		</div>
 				<!--2 ième colonne -->
 		<div class="col-md-2">
-			<h4>Location</h4>
+			<h3>Location</h3>
 				<a href="">Dépôt d'annonce</a><br>
 				<a href="">Trouver une location</a><br>		<!--3 ième colonne -->
 				<a href="">Recherche du lieu</a><br>
@@ -21,7 +23,7 @@
 		</div>
 
 		<div class="col-md-2">
-			<h4>Avis</h4>
+			<h3>Avis</h3>
 				<a href="">Dépôt d'annonce</a><br>
 				<a href="">Trouver une location</a><br>		<!--4 ième colonne -->
 				<a href="">Recherche du lieu</a><br>
@@ -29,7 +31,7 @@
 		</div>		
 
 		<div class="col-md-2">
-			<h4>Contact</h4>
+			<h3>Contact</h3>
 				<a href="">Dépôt d'annonce</a><br>
 				<a href="">Trouver une location</a><br>		<!--5 ième colonne -->
 				<a href="">Recherche du lieu</a><br>
