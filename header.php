b83d7c060c9a0004059aa8a7bce9d08b6cbee878
Alexandre HERRERO <alexandre.herrero@live.fr>
2016-10-18 15:57:18 +0200

    mod footer

--- header.php
+++ header.php
@@ -4,10 +4,10 @@
     <title><?php wp_title(); ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
     <link href="<?php bloginfo('url'); ?>/wp-content/themes/seloger/responsive.css" rel="stylesheet">
     <link href="<?php bloginfo('url'); ?>/wp-content/themes/seloger/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php bloginfo('url'); ?>/wp-content/themes/seloger/bootstrap/js/bootstrap.min.js" rel="stylesheet">
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -16,7 +16,7 @@
 
 <body>
 <section id="sec1">
-<nav id="navbar-primary" class="navbar-inverse" role="navigation">
+<nav id="navbar-primary" class="navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
