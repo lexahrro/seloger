7b5bcdc369acea7a224e7df96c5ceee8d299d2bb
Alexandre HERRERO <alexandre.herrero@live.fr>
2016-10-18 09:40:07 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    <link href="<?php bloginfo('url'); ?>/wp-content/themes/seloger/responsive.css" rel="stylesheet">
+</head>
+
+<body>
+</body>
+
+</html>
