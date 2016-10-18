858168ee4e6e3a8ad6b3c3d2e2bbf03753ead451
Charo <diridolloumarwan@gmail.com>
2016-10-18 11:43:00 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,42 @@
+<div class="container-footer">
+	<div id="footer" class="row slgrow">
+		<div class="col-md-4">
+			<H1>ouloulou</H1>		<!--Logo première colonne -->
+		</div>
+				
+		<div class="col-md-2">
+			<h4>Accueil</h4>
+				<a href="">Dépôt d'annonce</a><br>
+				<a href="">Trouver une location</a><br>
+				<a href="">Recherche du lieu</a><br>		<!--2 ième colonne -->
+				<a href="">Contact</a><br>
+		</div>
+				<!--2 ième colonne -->
+		<div class="col-md-2">
+			<h4>Location</h4>
+				<a href="">Dépôt d'annonce</a><br>
+				<a href="">Trouver une location</a><br>		<!--3 ième colonne -->
+				<a href="">Recherche du lieu</a><br>
+				<a href="">Contact</a><br>
+		</div>
 
+		<div class="col-md-2">
+			<h4>Avis</h4>
+				<a href="">Dépôt d'annonce</a><br>
+				<a href="">Trouver une location</a><br>		<!--4 ième colonne -->
+				<a href="">Recherche du lieu</a><br>
+				<a href="">Contact</a><br>
+		</div>		
 
-
-
+		<div class="col-md-2">
+			<h4>Contact</h4>
+				<a href="">Dépôt d'annonce</a><br>
+				<a href="">Trouver une location</a><br>		<!--5 ième colonne -->
+				<a href="">Recherche du lieu</a><br>
+				<a href="">Contact</a><br>
+		</div>				
+	</div>
+</div>
 
 
 
