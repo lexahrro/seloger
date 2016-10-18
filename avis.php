42d529afdfa67a303a09ddb520c7fe90ae06f3de
Charo <diridolloumarwan@gmail.com>
2016-10-18 18:01:50 +0200

    avis

--- avis.php
+++ avis.php
@@ -1,8 +1,54 @@
-<?php get_header(); ?>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="http://localhost/wordpress/wp-content/themes/seloger/responsive.css" rel="stylesheet">
+    <link href="http://localhost/wordpress/wp-content/themes/seloger/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://localhost/wordpress/wp-content/themes/seloger/bootstrap/js/bootstrap.min.js" rel="stylesheet">
+    <link href="http://localhost/wordpress/wp-content/themes/seloger/style.css"rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-	<section>
+</head>
+
+	<section id="avissec1">
+		<div class="container" id="container-interview">
+			<div class="row">
+				<div class="col-md-4" id="interview1">
+					<img src="img/bg.jpg"  alt="logo SLG" class="img-responsive clodo1"> <!--Photo interview 1-->
+				</div>
+				<div class="col-md-8" id="texte1"> <!-- Citation interview 1-->
+					<h4>Prénom:Jean</h4>
+					<h4>Nom:Delarue</h4>
+					<h4>Age:57 ans</h4>
+					<blockquote>
+     							<p>“J'ai vite commencé à faire la manche. Au début, je n'osais pas. Je paniquais. Je prenais la monnaie en main, je n'avais pas de pot.Grâce à Nepaseloger, j’ai faciliment trouvé un endroit pour passer mes nuits dans un confort longtemps recherché”</p>
+     							<small>de <cite>Jean Delarue</cite></small><!-- Citation du clodo1 -->
+					</blockquote>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-8" id="texte2"> <!-- Citation interview 2-->
+					<h4>Prénom:Jean</h4>
+					<h4>Nom:Caisse</h4>
+					<h4>Age:65 ans</h4>
+					<blockquote>
+     							<p>“Ah, en hiver! Oui, j'ai déjà craqué, en hiver. En arrivant sur mon banc, j'ai pleuré comme un enfant. Un soir, je me suis endormi, j'avais bu, je n'ai pas mis ma couverture, on m'a vidé les poches, pris mes papiers. Depuis, plus moyen d'en ravoir d'autres! Je suis un sans -papiers...francais.Nepaseloger, m’a donné une seconde identitée”</p><!-- Citation du clodo1 -->
+     							<small>de <cite>Jean Caisse</cite></small>
+					</blockquote>  <!-- Citation du clodo2 -->
+				</div>
+				<div class="col-md-4" id="interview2">
+					<img src="img/bg3.jpg"  alt="logo SLG" class="img-responsive clodo2" > <!--Photo interview 2-->
+				</div>
+			</div>
+		</div>
 
 
-<?php get_footer(); ?>
+
+
+	</section>
+
+
+
 
 
